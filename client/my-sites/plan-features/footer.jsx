9b057f240767bd925c9c3008333ff4db02bd3d35
Kerry Liu <kerryliu@gmail.com>
2016-06-30 16:16:27 -0700

    Plans: add the longest plan  description as a hidden span so heights match

--- client/my-sites/plan-features/footer.jsx
+++ client/my-sites/plan-features/footer.jsx
@@ -4,6 +4,8 @@
 import { localize } from 'i18n-calypso';
 import noop from 'lodash/noop';
 import React, { PropTypes } from 'react';
+import filter from 'lodash/filter';
+import reduce from 'lodash/reduce';
 
 /**
  * Internal dependencies
@@ -11,19 +13,51 @@ import React, { PropTypes } from 'react';
 import Button from 'components/button';
 import Gridicon from 'components/gridicon';
 import classNames from 'classnames';
+import { plansList } from 'lib/plans/constants';
+
+function getLongestDescription( planName ) {
+	if ( ! planName ) {
+		return null;
+	}
+	const isJetpack = planName.indexOf( 'jetpack' ) === 0;
+	const plans = Object.keys( plansList );
+
+	return reduce(
+		filter( plans, ( plan ) => {
+			const isPlanJetpack = plan.indexOf( 'jetpack' ) === 0;
+			return isJetpack ? isPlanJetpack : ! isPlanJetpack;
+		} ),
+		( longestDescription, plan ) => {
+			const currentDescription = plansList[ plan ].getDescription();
+			return currentDescription.length > longestDescription.length
+				? currentDescription : longestDescription;
+		},
+		''
+	);
+}
 
 const PlanFeaturesFooter = ( {
 	available = true,
 	current = false,
 	description,
 	onUpgradeClick = noop,
-	translate
+	planType,
+	translate,
 } ) => {
 	const classes = classNames( 'plan-features__footer', { 'has-description': !! description } );
-
+	const longestDescription = getLongestDescription( planType );
 	return (
 		<footer className={ classes }>
-			{ description && <p className="plan-features__footer-desc">{ description }</p> }
+			{ description &&
+				<p className="plan-features__footer-desc">
+					<span className="plan-features__footer-desc-text">
+						{ description }
+					</span>
+					<span className="plan-features__footer-desc-longest">
+						{ longestDescription }
+					</span>
+				</p>
+			}
 			<div className="plan-features__footer-buttons">
 				{
 					current
@@ -47,7 +81,8 @@ PlanFeaturesFooter.propTypes = {
 	current: PropTypes.bool,
 	available: PropTypes.bool,
 	description: PropTypes.string,
-	onUpgradeClick: PropTypes.func
+	onUpgradeClick: PropTypes.func,
+	planType: PropTypes.string
 };
 
 export default localize( PlanFeaturesFooter );
